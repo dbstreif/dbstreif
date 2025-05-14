9466f3c93710d116c981fd1ca2cd0eb085094108
Dom13377 <streif.dominic@gmail.com>
2025-05-14 13:31:25 -0700

    Uploaded website to main user repo

---
+++ src/Navbar.jsx
@@ -0,0 +1,23 @@
+import './Navbar.css'
+import { Link } from "react-router-dom"
+
+function Navbar () {
+    return (
+        <nav className="navbar">
+            <Link className="home" to="/react-portfolio/">
+                Home 
+            </Link>
+            <Link className="projects" to="/react-portfolio/projects">
+                Projects
+            </Link>
+            <Link className="resume" to="/react-portfolio/resume">
+                Resume
+            </Link>
+            <Link className="articles" to="/react-portfolio/articles">
+                Articles
+            </Link>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Projects from './Projects.jsx'
+import Resume from './Resume.jsx'
+import Articles from './Articles.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: "/react-portfolio/",
+        element: <App/>,
+    },
+    {
+        path: "/react-portfolio/projects",
+        element: <Projects/>,
+    },
+    {
+        path: "/react-portfolio/resume",
+        element: <Resume/>,
+    },
+    {
+        path: "/react-portfolio/articles",
+        element: <Articles/>
+    },
+    {
+        path: "/react-portfolio/*",
+        element: <App/>
+    },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+        <RouterProvider router={router}/>
+  </StrictMode>,
+)
